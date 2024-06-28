669cfd69a057472a709e393d5e9a569eb83b55de
sambit-giri <sambit.giri@gmail.com>
2024-06-28 07:17:34 +0200

    zion

--- src/toolslyman/ionized_igm.py
+++ src/toolslyman/ionized_igm.py
@@ -1,47 +1,55 @@
 import numpy as np
 from glob import glob
 from tqdm import tqdm
-# import astropy
-# from astropy import constants, units
-# from astropy.cosmology import Planck18
+import astropy
+from astropy import constants, units
+from astropy.cosmology import Planck18
 
 def make_zion_field(filenames_or_dict, xion_thres=0.99, file_redshifts=None, reading_function=None):
-    '''
-    Make a lightcone from xfrac, density or dT data. Replaces freq_box.
-    
+    """
+    Create a z_ion field where pixels define the redshift when it was ionised.
+
     Parameters:
-        filenames_or_dict (string or dict): The coeval cubes of ionization fields.
-            Can be either any of the following:
-            
-                - An array with the file names
-                
-                - A dictionary containing ionization fields with redshifts as keys
+    -----------
+    filenames_or_dict : str or dict
+        The coeval cubes of ionization fields. Can be one of the following:
+        - An array of file names
+        - A dictionary containing ionization fields with redshifts as keys
         
-        xion_thres (float): The threshold value of x_HII to use to define completion of ionisation.
-        file_redshifts (array): The redshifts of the coeval cubes if provided as file names.
-        reading_function (function): a function to read the files if file names are provided.
+    xion_thres : float, optional
+        The threshold value of x_HII to use to define completion of ionization. Default is 0.99.
+        
+    file_redshifts : array-like, optional
+        The redshifts of the coeval cubes if provided as file names.
+        
+    reading_function : function, optional
+        A function to read the files if file names are provided.
 
     Returns:
-        zion array
-    '''
+    --------
+    zion : ndarray
+        The redshift of ionization field.
+    """
     if isinstance(filenames_or_dict, dict):
         xfrac_dict = filenames_or_dict
-        xfrac_reds = np.array([ke for ke in xfrac_dict.keys()])
+        xfrac_reds = np.array(list(xfrac_dict.keys()))
     else:
-        assert len(file_redshifts) == len(filenames_or_dict)
+        assert len(file_redshifts) == len(filenames_or_dict), "Number of file names and redshifts must match."
         xfrac_reds = file_redshifts
         xfrac_dict = {}
-        print('Reading ionisation data...')
-        for ii,zi in tqdm(enumerate(xfrac_reds)):
+        print('Reading ionization data...')
+        for ii, zi in tqdm(enumerate(xfrac_reds)):
             xfrac_dict[zi] = reading_function(filenames_or_dict[ii])
         print('...done')
-    print('MAking the z_ion field...')
+        
+    print('Making the z_ion field...')
     zion = np.ones_like(xfrac_dict[xfrac_reds[0]])
     xfrac_reds = np.sort(xfrac_reds)
-    for ii,zi in tqdm(enumerate(xfrac_reds)):
+    for ii, zi in tqdm(enumerate(xfrac_reds)):
         xi = xfrac_dict[zi]
-        zion[xi>=xion_thres] = zi
-    print('...done') 
+        zion[xi >= xion_thres] = zi
+    print('...done')
+    
     return zion
 
 def gas_temp_inside_ionized_igm_single(z, dens, z_ion, dens_ion, cosmo=None):
